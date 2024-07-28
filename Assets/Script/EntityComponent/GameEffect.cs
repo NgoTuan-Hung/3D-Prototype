dd8e1254b74e5833c8d7ef9f57a7c7032d79a948
unknown <hung.nt207608@sis.hust.edu.vn>
2024-07-28 19:55:08 +0700

    .

--- Assets/Script/EntityComponent/GameEffect.cs
+++ Assets/Script/EntityComponent/GameEffect.cs
@@ -6,6 +6,7 @@ using UnityEngine.VFX;
 public class GameEffect : MonoBehaviour
 {
     [SerializeField] new private ParticleSystem particleSystem;
+    private bool particleSystemBool = false;
     private ParticleSystemEvent particleSystemEvent;
     private bool particleSystemEventBool = false;
     private VisualEffect visualEffect;
@@ -15,6 +16,8 @@ public class GameEffect : MonoBehaviour
     [SerializeField] private bool disableAfterTime = false;
     [SerializeField] private float disableTime = 2f;
     [SerializeField] private List<Animator> animators = new List<Animator>();
+    private Animator animator;
+    private bool animatorBool = false;
     public ParticleSystem ParticleSystem { get => particleSystem; set => particleSystem = value; }
     internal ParticleSystemEvent ParticleSystemEvent { get => particleSystemEvent; set => particleSystemEvent = value; }
     public CollideAndDamage CollideAndDamage { get => collideAndDamage; set => collideAndDamage = value; }
@@ -24,22 +27,29 @@ public class GameEffect : MonoBehaviour
     public bool VisualEffectBool { get => visualEffectBool; set => visualEffectBool = value; }
     public bool DisableAfterTime { get => disableAfterTime; set => disableAfterTime = value; }
     public float DisableTime { get => disableTime; set => disableTime = value; }
+    public Animator Animator { get => animator; set => animator = value; }
+    public bool TravelToDirectionBool { get => travelToDirectionBool; set => travelToDirectionBool = value; }
 
     void Awake()
     {
-        if (!alreadySetOriginal)
+        if ((particleSystem = GetComponentInChildren<ParticleSystem>()) != null)
         {
-            alreadySetOriginal = true;
-            var main = particleSystem.main;
-            particleSystemStartSizeXMultiplierOriginal = main.startSizeXMultiplier;
-            particleSystemStartSizeYMultiplierOriginal = main.startSizeYMultiplier;
-            particleSystemStartSizeZMultiplierOriginal = main.startSizeZMultiplier;
-            particleSystemStartLifetimeOriginal = main.startLifetime.constantMax;
+            particleSystemBool = true;
+            if (!alreadySetOriginal)
+            {
+                alreadySetOriginal = true;
+                var main = particleSystem.main;
+                particleSystemStartSizeXMultiplierOriginal = main.startSizeXMultiplier;
+                particleSystemStartSizeYMultiplierOriginal = main.startSizeYMultiplier;
+                particleSystemStartSizeZMultiplierOriginal = main.startSizeZMultiplier;
+                particleSystemStartLifetimeOriginal = main.startLifetime.constantMax;
+            }
         }
 
         if ((particleSystemEvent = GetComponentInChildren<ParticleSystemEvent>()) != null) particleSystemEventBool = true;
         if ((visualEffect = GetComponentInChildren<VisualEffect>()) != null) visualEffectBool = true;
         if ((collideAndDamage = GetComponentInChildren<CollideAndDamage>()) != null) collideAndDamageBool = true;
+        if ((animator = GetComponent<Animator>()) != null) animatorBool = true;
     }
 
     void OnEnable()
@@ -59,6 +69,102 @@ public class GameEffect : MonoBehaviour
     {
         yield return new WaitForSeconds(disableTime);
         gameObject.SetActive(false);
+    }
+
+    public delegate void TriggerActionDelegate();
+    public TriggerActionDelegate triggerActionDelegate;
+    public void TriggerActionWithCondition(bool distanceToTarget, GameObject targetGameObject, float distance, bool timer, float time)
+    {
+        StartCoroutine(TriggerActionWithConditionCoroutine(distanceToTarget, targetGameObject, distance, timer, time));
+    }
+
+    public IEnumerator TriggerActionWithConditionCoroutine(bool distanceToTarget, GameObject targetGameObject, float distance, bool timer, float time)
+    {
+        if (distanceToTarget)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Time.fixedDeltaTime);
+
+                if (Vector3.Distance(targetGameObject.transform.position, transform.position) <= distance)
+                {
+                    triggerActionDelegate?.Invoke();
+                    triggerActionDelegate = null;
+                    break;
+                }
+            }
+        }
+        else if (timer)
+        {
+            yield return new WaitForSeconds(time);
+            triggerActionDelegate?.Invoke();
+            triggerActionDelegate = null;
+        }
+    }
+
+    public delegate void TriggerActionDelegate1();
+    public TriggerActionDelegate triggerActionDelegate1;
+    public Coroutine triggerActionWithConditionCoroutine1;
+    public void TriggerActionWithCondition1(bool distanceToTarget, GameObject targetGameObject, float distance, bool timer, float time)
+    {
+        triggerActionWithConditionCoroutine1 = StartCoroutine(TriggerActionWithConditionCoroutine1(distanceToTarget, targetGameObject, distance, timer, time));
+    }
+
+    public IEnumerator TriggerActionWithConditionCoroutine1(bool distanceToTarget, GameObject targetGameObject, float distance, bool timer, float time)
+    {
+        if (distanceToTarget)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Time.fixedDeltaTime);
+
+                if (Vector3.Distance(targetGameObject.transform.position, transform.position) <= distance)
+                {
+                    triggerActionDelegate1?.Invoke();
+                    triggerActionDelegate1 = null;
+                    break;
+                }
+            }
+        }
+        else if (timer)
+        {
+            yield return new WaitForSeconds(time);
+            triggerActionDelegate1?.Invoke();
+            triggerActionDelegate1 = null;
+        }
+    }
+
+    private bool travelToDirectionBool = false;
+    public void TravelToDirection(Vector3 direction, float speed)
+    {
+        StartCoroutine(TravelToDirectionCoroutine(direction, speed));
+    }
+
+    public IEnumerator TravelToDirectionCoroutine(Vector3 direction, float speed)
+    {
+        travelToDirectionBool = true;
+        Vector3 finalDirection = direction.normalized * speed;
+        while (travelToDirectionBool)
+        {
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            transform.position += finalDirection * Time.fixedDeltaTime;
+        }
+    }
+
+    public delegate void AnimationEvent1Delegate();
+    public AnimationEvent1Delegate animationEvent1Delegate;
+    public void AnimationEvent1()
+    {
+        animationEvent1Delegate?.Invoke();
+        animationEvent1Delegate = null;
+    }
+
+    public delegate void AnimationEvent2Delegate();
+    public AnimationEvent2Delegate animationEvent2Delegate;
+    public void AnimationEvent2()
+    {
+        animationEvent2Delegate?.Invoke();
+        animationEvent2Delegate = null;
     }
 
     public delegate void OnDisableDelegate();
--- Assets/Script/Weapon/SkeletonSwordSkillNonstopThrust.cs
+++ Assets/Script/Weapon/SkeletonSwordSkillNonstopThrust.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 class SkeletonSwordSkillNonstopThrust : WeaponSubSkill
 {
     private static ObjectPool effectPool;
+    private static ObjectPool skeletonThrustingEffectPool;
     public override void Awake() 
     {
         base.Awake();
         GameObject effectPrefab = Resources.Load("Effect/NonstopThrust") as GameObject;
         effectPool ??= new ObjectPool(effectPrefab, 20, new PoolArgument(typeof(GameEffect), PoolArgument.WhereComponent.Self));
+
+        GameObject skeletonThrustingEffectPrefab = Resources.Load("Effect/SkeletonThrustingEffect") as GameObject;
+        skeletonThrustingEffectPool ??= new ObjectPool(skeletonThrustingEffectPrefab, 20, new PoolArgument(typeof(GameEffect), PoolArgument.WhereComponent.Self));
 
         SubSkillChangableAttributes.AddRange(new SubSkillChangableAttribute[] {coolDown});
 
@@ -34,15 +38,56 @@ class SkeletonSwordSkillNonstopThrust : WeaponSubSkill
             CanUse = false;
             
             GameEffect gameEffect = effectPool.PickOne().GameEffect;
-            gameEffect.transform.position = CustomMonoBehavior.SkillCastOriginPoint.transform.position;
+            gameEffect.VisualEffect.Stop();
 
-            gameEffect.CollideAndDamage.CollideExcludeTags = CustomMonoBehavior.AllyTags;
-            float angle = Quaternion.LookRotation(subSkillParameter.Target.transform.position - gameEffect.transform.position).eulerAngles.y;
-            gameEffect.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
-            gameEffect.VisualEffect.Play();
+            GameEffect skeletonThrustingEffect = skeletonThrustingEffectPool.PickOne().GameEffect;
+            skeletonThrustingEffect.transform.position = transform.position;
+            skeletonThrustingEffect.Animator.speed = 0;
+
+            Vector3 travelToDirection = subSkillParameter.Target.transform.position - skeletonThrustingEffect.transform.position;
+            float angle = Quaternion.LookRotation(travelToDirection).eulerAngles.y;
+            skeletonThrustingEffect.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+            
+            skeletonThrustingEffect.TravelToDirection(travelToDirection, 5);
+            skeletonThrustingEffect.triggerActionDelegate += () => TriggerActionDelegateParam(skeletonThrustingEffect);
+            skeletonThrustingEffect.triggerActionDelegate1 += () => TriggerActionDelegate1Param(skeletonThrustingEffect);
+            skeletonThrustingEffect.animationEvent1Delegate += () => AnimationEvent1DelegateParam(gameEffect, skeletonThrustingEffect);
+            skeletonThrustingEffect.animationEvent2Delegate += () => AnimationEvent2DelegateParam(skeletonThrustingEffect);
+            skeletonThrustingEffect.TriggerActionWithCondition1(false, null, 0, true, 5f);
+            skeletonThrustingEffect.TriggerActionWithCondition(true, subSkillParameter.Target.gameObject, 2f, false, 0);
 
             StartCoroutine(StopCoroutine());
         }
+    }
+
+    public void TriggerActionDelegateParam(GameEffect skeletonThrustingEffect)
+    {
+        skeletonThrustingEffect.Animator.speed = 1;
+        skeletonThrustingEffect.TravelToDirectionBool = false;
+        skeletonThrustingEffect.StopCoroutine(skeletonThrustingEffect.triggerActionWithConditionCoroutine1);
+    }
+
+    public void TriggerActionDelegate1Param(GameEffect skeletonThrustingEffect)
+    {
+        skeletonThrustingEffect.gameObject.SetActive(false);
+    }
+
+    public void AnimationEvent1DelegateParam(GameEffect gameEffect, GameEffect skeletonThrustingEffect)
+    {
+        gameEffect.transform.position = skeletonThrustingEffect.transform.position + new Vector3(0, 1, 0);
+        gameEffect.CollideAndDamage.CollideExcludeTags = CustomMonoBehavior.AllyTags;
+        gameEffect.transform.rotation = skeletonThrustingEffect.transform.rotation;
+        gameEffect.VisualEffect.Play();
+        gameEffect.triggerActionDelegate += () => 
+        {
+            gameEffect.gameObject.SetActive(false);
+        };
+        gameEffect.TriggerActionWithCondition(false, null, 0, true, 5f);
+    }
+    
+    public void AnimationEvent2DelegateParam(GameEffect skeletonThrustingEffect)
+    {
+        skeletonThrustingEffect.gameObject.SetActive(false);
     }
 
     public IEnumerator StopCoroutine()
