45272e217981941762d82c3ac0e9c55c3235021f
unknown <hung.nt207608@sis.hust.edu.vn>
2024-07-29 12:03:55 +0700

    .

---
+++ Assets/Script/EntityComponent/JumpableObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpableObject : MonoBehaviour
+{
+    public BoxCollider jumpCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
