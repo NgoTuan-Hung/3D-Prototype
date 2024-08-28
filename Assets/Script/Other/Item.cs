16b51ad122bb6e5039060b7867f8a659da474e43
unknown <hung.nt207608@sis.hust.edu.vn>
2024-08-28 13:32:47 +0700

    .

--- Assets/Script/Other/Item.cs
+++ Assets/Script/Other/Item.cs
@@ -1,22 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public enum ItemType {SkillBook}
+    public enum ItemType {RandomSkillBook}
     public ItemType itemType;
     private bool interactable = true;
     private string skillName;
+    private Animator animator;
+    private ObjectPool objectPool;
 
     void Awake()
     {
-        OnInteract = itemType switch
+        switch (itemType)
         {
-            ItemType.SkillBook => SkillBookInteract,
-            _ => null
-        };
+            case ItemType.RandomSkillBook:
+                skillName = "SkillBook";
+                OnInteract = SkillBookInteract;
+                animator = GetComponent<Animator>();
+                objectPool = new ObjectPool
+                (
+                    Resources.Load("Effect/Book/BookTrail") as GameObject,
+                    20, new PoolArgument(typeof(GameEffect), PoolArgument.WhereComponent.Self)
+                );
+                AnimationClip animationClip = animator.runtimeAnimatorController.animationClips.First
+                (
+                    clip => clip.name.Equals("BookAnim")
+                );
+                animationClip.AddEvent(new AnimationEvent()
+                {
+                    functionName = "RandomSkillBookSpawnVFX",
+                    time = 0.915f
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
+    private delegate void FixedUpdateDelegate();
+    private FixedUpdateDelegate FixedUpdateDelegateInstance;
+    void FixedUpdate()
+    {
+        FixedUpdateDelegateInstance?.Invoke();
     }
 
     public delegate void InteractableDelegate(Collision collision);
@@ -28,8 +58,32 @@ public class Item : MonoBehaviour
         interactedCustomMonoBehavior = GlobalObject.Instance.GetCustomMonoBehavior(collision.gameObject);
     }
 
+    public void RandomSkillBookSpawnVFX()
+    {
+        GameEffect gameEffect;
+        gameEffect = objectPool.PickOne().GameEffect;
+        gameEffect.transform.position = transform.position;
+        gameEffect.VisualEffect.Play();
+        
+        StartCoroutine(Coroutine1(5f, () => gameEffect.gameObject.SetActive(false)));
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         
+    }
+
+    private delegate void AnimationEventDelegate();
+    private AnimationEventDelegate AnimationEventDelegateInstance;
+    public void AnimationEvent1()
+    {
+        AnimationEventDelegateInstance?.Invoke();
+    }
+
+    public IEnumerator Coroutine1(float time, Action action)
+    {
+        yield return new WaitForSeconds(time);
+        
+        action();
     }
 }
