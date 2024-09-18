36998478203f0decba2bc757cb939cd78b451691
unknown <hung.nt207608@sis.hust.edu.vn>
2024-09-18 20:01:30 +0700

    .

---
+++ Assets/Script/EntityComponent/MeshStructure.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshStructure : MonoBehaviour
+{
+    private List<MeshRenderer> meshRenderers;
+    private List<SkinnedMeshRenderer> skinnedMeshRenderers;
+}
